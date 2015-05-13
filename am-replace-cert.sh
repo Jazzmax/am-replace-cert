c350a24589431518ba03d047e72ebfa7394fdacc
Max Siyazov <mappett.show@gmail.com>
2015-05-13 06:51:20 +0100

    version 1.2
    1.2 + added expiration check

--- am-replace-cert.sh
+++ am-replace-cert.sh
@@ -1,82 +1,74 @@
 #!/bin/bash
-VER=1.1
+VER=1.2
 ##########################################################
+## This script is to assist with replacing a server certificate on AM 7.1 appliance
+##
+##########################################################
+# Usage: replace-cert.sh [OPTION] <alias>
+# Specify only <alias> without parameters to do an whole certificate replacement procedure.  
+# This will:	1. generate a new key pair with <alias>
+#				2. create a CSR
+#				3. import the full chain of certificates 
+#				4. configure servers with the new server certificate
 #
-# This script will assist to replace a server certificate on AM 7.1 appliance
-##########################################################
-
-##########################################################
-# Usage: ./replace-cert.sh
-#  options:  -import : Imports new certificates 
-#            -config : To do. Reconfigurates Weblogic to use a new server certificate.
-#			 -delete : delete the cert from the all keystores 
-#			 -list	 : list keystores
+#  options:  -config : Reconfigures the Weblogic to use a server certificate with <alias>
+#			 -delete : delete the cert with <alias> from the all keystores (identity, root, JDK trusted)
+#			 -list	 : list the content of all keystores / with argument will show the entry with <alias>  
 #			 
 ##########################################################
 # Author : Maxim Siyazov
 # Version History
 # 0.80  - Initial version
-# 0.91  - added +list keystores, +delete alias from all keystores, +import certs, many other improvements 
+# 0.91  + list keystores, 
+#		+ delete alias from all keystores, 
+#		+ import certs, 
+#		* many other improvements 
 # 0.92  - some bug fixes
 # 0.93  - changed mechanism of loading a chain of certificates.
-# 1.0   - the initial release. *improved configuring the AM servers, added a backup/restore the WebLogic config
-# 1.1   - added check of the Signature Algorithm (AM-26699), +bug fixes.
+# 1.0   - the initial release. 
+#		* improved configuring the AM servers
+#		+ added a backup/restore the WebLogic config
+# 1.1   + added check of the Signature Algorithm (AM-26699) 
+#		* bug fixes
+# 1.2	+ added expiration check
 # -------------------------------
 # TO DO
-# - Expiration check
 # - 
-
-###########################################################
-# Prompt a question and read answer
-# Param 1: Question
-# Param 2: default answer
-###########################################################
-promptYesNo(){
-
-  echo -n "$1 (y/n) [$2] "
-  read answer
-  answer=`echo $answer | tr [A-Z] [a-z]`
-  if [ -z "$answer" ]; then
-    answer=$2
-  fi
-  if [ "xy" = "x$answer" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
+# - 
 
 ###########################################################
 # Prints the usage and exits
 #
 usage() {
 
-    echo -e "Usage: replace_cert.sh {-gencsr|-import|-config|-list|-delete}"
-    echo -e "This script should be run as rsaadmin\n"
-	echo -e  "   -gencsr <alias>"
-	echo -e  "Generating a new key pair and a Certificate Signing Request\n"
-	echo -e  "   -import <alias>"
-	echo -e  "Importing the full chain of certificates into keystores\n"
-	echo -e  "   -config <alias>"
-	echo -e  "Configuring the RSA AM to use a new certificate\n"
-	echo -e  "   -list [<alias>]"
-	echo -e  "List a certificate with <alias> or all certificate in the keystores\n"
-	echo -e  "   -delete <alias>"
-	echo -e  "Delete the <alias> from all keystores\n"
+echo -e "Usage: replace_cert.sh [OPTION] alias\n
+This script should be run as \"rsaadmin\"\n
+    <alias> 		- whole certificate replacement procedure
+				1. generate a key pair
+				2. create a CSR
+				3. import certificates
+				4. configure servers\n
+   -config <alias>	- configuring the RSA AM to use a certificate with <alias>
+   
+   -list [<alias>]	- list a certificate with <alias> or all certificate in the identity and root keystores
+   
+   -delete <alias>	- delete the <alias> from all keystores (identity, root, JKD trusted)\n"
 
     exit 1
 } # End of usage
-
 
 
 ############################################################
 # Generating New Keys and a Certificate Signing Request
 # 
 gencsr(){
+echo -e "Generating a key pair with the alias name $ALIAS...\n "
 
 $RSAAM_HOME/utils/rsautil manage-ssl-certificate --genkey --alias "$ALIAS" --dname "CN=$FQDN" --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks -m $MASTER_PW --storepass $ID_STORE_PW
 
+echo -e "Creating a CSR...\n"	
 $RSAAM_HOME/utils/rsautil manage-ssl-certificate --certreq --alias "$ALIAS" --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks --csr-file "/tmp/${ALIAS}_csr.pem" -m $MASTER_PW --storepass $ID_STORE_PW
+
 
 if [ ! -f "/tmp/${ALIAS}_csr.pem" ]; then
   echo "CSR not generated in /tmp/. Exiting"
@@ -85,17 +77,15 @@ fi
 
 chmod 777 "/tmp/${ALIAS}_csr.pem"
 
-echo "Please submit the request file /tmp/${ALIAS}_csr.pem  to your certificate authority."
-echo "/tmp/${ALIAS}_csr.pem"
+echo "Please submit the PKCS#10 request below or file /tmp/${ALIAS}_csr.pem to your certificate authority."
 cat "/tmp/${ALIAS}_csr.pem"
 
-echo "Before completing the procedure, save the CA root certificate (and any intermediate CA certificates), and \ the signed server certificate you received from the certificate authority in a location of \
-your choosing on the Authentication Manager server, /tmp/certs/ by default. \
-You will be prompted for this location during the procedures. "
+echo -e "\nBefore completing the procedure, save the CA root certificate (and any intermediate CA certificates), and 
+the signed server certificate you received from the certificate authority in ${CERTSPATH}. \n"
 }
 
 ############################################################
-# Importing the full chain of certificates
+# Importing the full chain of certificates stored in CERTS[]
 #
 ############################################################
 import_certs(){
@@ -127,9 +117,8 @@ done
 
 echo "Importing the signed server certificate ${CERTS[0]} into the server keystore"
 	$RSAAM_HOME/utils/rsautil manage-ssl-certificate --import --alias "${ALIAS}" --cert-file "${CERTSPATH}${CERTS[0]}" --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks -m $MASTER_PW --storepass $ID_STORE_PW	
-
-
 }
+
 ############################################################
 # Configuring the RSA Authentication Manager Servers
 # Param 1: certificate alias
@@ -190,8 +179,6 @@ if [ $? -ne 0 ]; then
 fi
  
 echo
-echo "Done"
-pause
 	
 # Configuring the RSA Authentication Manager Proxy Server:
 echo "Configuring the RSA Authentication Manager Proxy Server..."
@@ -201,8 +188,6 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 echo 
-echo "Done"
-pause
 
 # Configuring the RSA Authentication Manager:
 echo "Configuring the RSA Authentication Manager..."
@@ -212,10 +197,9 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 echo 
-echo "Done"	
 
 echo 
-echo "Restarting the RSA Authentication Manager Servers..."
+echo "Restarting the RSA Authentication Manager Servers... Please wait this may take up to 10 minutes."
 	
 $RSAAM_HOME/server/rsaam restart allnoradius
  	
@@ -245,8 +229,15 @@ echo "DONE. All RSA AM services are running. "
 ############################################################
 # Delete a cert from all keystores
 # Param 1: cert alias
+############################################################
 delete_cert(){
 	CERT_ALIAS=$1
+	
+	promptYesNo "Are you sure you want to delete \"${CERT_ALIAS}\" from all key stores?" n
+		if [ $? -eq 0 ]; then
+			echo "Exiting."
+			exit 1
+		fi		
 	
 	echo "Deleting $CERT_ALIAS from the  identity keystore "
 	
@@ -264,7 +255,7 @@ delete_cert(){
 }
 
 ############################################################
-# Get keystore passwords
+# Get keystores passwords
 # 
 get_passwords(){
 	
@@ -331,6 +322,26 @@ echo "$RESOUT" | grep "Certificate[1]"
 }
 
 ###########################################################
+# Prompt a question and read answer
+# Param 1: Question
+# Param 2: default answer
+###########################################################
+promptYesNo(){
+
+  echo -n "$1 (y/n) [$2] "
+  read answer
+  answer=`echo $answer | tr [A-Z] [a-z]`
+  if [ -z "$answer" ]; then
+    answer=$2
+  fi
+  if [ "xy" = "x$answer" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+###########################################################
 # Main
 ###########################################################
 
@@ -339,6 +350,11 @@ date
 echo "Starting replace-cert.sh version $VER"
 echo "######################################"
 
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
+fi
+
 if [ "`whoami`" != "rsaadmin" ]; then
   echo "This script should be run as rsaadmin"
   exit 1
@@ -346,6 +362,12 @@ fi
 
 RSAAM_HOME="/usr/local/RSASecurity/RSAAuthenticationManager"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Colouring output
+COL_BLUE="\x1b[34;01m"
+COL_GREEN="\x1b[32;01m"
+COL_RED="\x1b[31;01m"
+COL_RESET="\x1b[39;49;00m"
 
 cd $RSAAM_HOME/utils
 
@@ -370,13 +392,7 @@ if [ $# -gt 0 ]; then
 	get_passwords
 	delete_cert "$2"
     exit 0	 
-#  elif [ "$1" = "-gencsr" ]; then
-#	if [ -z "$2" ]; then usage && exit 1 
-#	fi  
-#    get_passwords
-#    gencsr "$2"
-#    exit 0
-	elif [ "$1" = "-list" ]; then
+  elif [ "$1" = "-list" ]; then
 	get_passwords
 	list_certs "$2"
      exit 0	 	 
@@ -385,11 +401,7 @@ if [ $# -gt 0 ]; then
 	fi
     get_passwords	
     configureServers "$2"
-    exit 0
-#  elif [ "$1" = "-instore" ]; then
-#    get_passwords
-#    instore "$2"
-#    exit 0	
+    exit 0 
   fi
 else usage && exit 1
 fi
@@ -397,23 +409,32 @@ fi
 get_passwords
 
 echo "com.rsa.identity.store: $ID_STORE_PW"
-echo "com.rsa.root.store: $ROOT_STORE_PW"
+echo -e "com.rsa.root.store: $ROOT_STORE_PW\n"
 
-#echo "Enter the alias for a new server certificate: "
-#read ALIAS
+CURR_CERT=`grep -oPm1 "(?<=<server-private-key-alias>)[^<]+" $RSAAM_HOME/server/config/config.xml`
+echo -n "The current server certificate "
+
+$RSAAM_HOME/utils/rsautil manage-ssl-certificate --list  --alias "$CURR_CERT" --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks -m $MASTER_PW --storepass $ID_STORE_PW
+echo
 
 ALIAS=$1
 
-echo "Checking to see if the certificate alias ${ALIAS} exists."
-$RSAAM_HOME/utils/rsautil manage-ssl-certificate --list --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks -m $MASTER_PW --storepass $ID_STORE_PW | grep "$ALIAS"
+echo -n "Checking to see if the certificate alias \"${ALIAS}\" exists. "
+$RSAAM_HOME/utils/rsautil manage-ssl-certificate --list --keystore $RSAAM_HOME/server/security/$MACHINE_NAME.jks -m $MASTER_PW --storepass $ID_STORE_PW | grep "$ALIAS" >null
 	if [ $? -eq 1 ]; then
-		gencsr
+		echo -e ${COL_GREEN}"Does not exist."${COL_RESET}
+		promptYesNo "Do you want to generate a new signing certificates request?" y
+		if [ $? -eq 0 ]; then
+			echo "Exiting."
+			exit 1
+		fi	
+	gencsr
 	else
-		echo "Alias $ALIAS already exists."
+		echo -e "${COL_BLUE}The alias already exists!${COL_RESET}"
 	fi
-		
-echo "If you have received a reply from the CA make sure you have saved the server certificate, CA root certificate, and intermediate CA certificates (if any) into ${CERTSPATH}."
-promptYesNo "Do you want to import certificates for the alias $ALIAS " y
+
+echo -e "If you have received a reply from the CA make sure you have saved the server certificate, CA root certificate, and intermediate CA certificates (if any) into ${CERTSPATH}."
+promptYesNo "Do you want to import certificates for the alias \"${ALIAS}\"" y
 	if [ $? -eq 0 ]; then
 		echo "OK. Exiting."
 		exit 1
@@ -423,62 +444,77 @@ echo -e "\nFiles in the $CERTSPATH:\n"
 ls -1 ${CERTSPATH}
 echo	
 # File name of the Server certificate
-echo "Enter a file name of the new server certificate:"
+echo "Enter a file name of new server certificate to import with the alias \"$ALIAS\":"
 read CERTS[0] 
+if [ ! -f "${CERTSPATH}${CERTS[0]}" ]; then
+	echo -e "${COL_RED}File not found!${COL_RESET}"
+	exit 1
+fi
 
-CERT_INFO=`openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -subject -issuer -serial -dates `
+CERT_INFO=`openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -subject -issuer -serial -dates`
 
-echo -e "\n${CERT_INFO}\n"
+echo -e "\n${CERT_INFO}"
 
-#SRV_CERT_ISSUER=`openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -issuer | sed 's/^issuer= //'`
 SRV_CERT_ISSUER=`echo "${CERT_INFO}" | grep "issuer=" | sed 's/^issuer= //'`
 SRV_CERT_SUBJECT=`echo "${CERT_INFO}" | grep "subject=" | sed 's/^subject= //'`
-
 SRV_CERT_SIGALG=`openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -text | grep -m 1 "Signature Algorithm:" | sed -e 's/^ *//g' -e 's/^Signature Algorithm: //'`
-echo -e "Signature Algorithm = $SRV_CERT_SIGALG\n"
+
 # check AM-26699 
 if [ "$SRV_CERT_SIGALG" != "sha1WithRSAEncryption" ]; then 
-	echo -e "Only sha1WithRSAEncryption signature algorithm supported. Exiting."
+    echo -e "signature algorithm =${COL_RED} $SRV_CERT_SIGALG ${COL_RESET}\n"
+	echo -e "${COL_RED}Error: Only sha1WithRSAEncryption signature algorithm supported.$COL_RESET Exiting."
 	exit 1
+else 
+	echo -e "signature algorithm = $SRV_CERT_SIGALG\n"
 fi	
 
+
 # Check to see if CN=<fqdn> 
-openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -subject | grep -e "CN=$FQDN"
+openssl x509 -in "${CERTSPATH}${CERTS[0]}" -noout -subject | grep -e "CN=$FQDN" > /dev/null
     if [ $? -ne 0 ]; then
-      echo "Wrong server certificate. Common Name (CN) of the server certificate must be $FQDN. Exiting."
+      echo -e $COL_RED"Wrong server certificate. Common Name (CN) of the server certificate must be $FQDN. Exiting."$COL_RESET
       exit 1
     fi
 
+openssl x509 -checkend $(( 86400 * 10 )) -in "${CERTSPATH}${CERTS[0]}" > /dev/null
+    if [ $? != 0 ]; then
+        echo -e $COL_RED"==> Certificate ${CERTS[0]} is about to expire soon. Exiting."$COL_RESET
+		exit 1
+    fi	
+	
+	
 FULLCHAIN=0  					# No full chain found
 CERT_ISSUER=$SRV_CERT_ISSUER 	
 
 i=1
 while [ $FULLCHAIN -eq 0 ]; do
-    echo -e "Files in the $CERTSPATH \n------------"
-	echo -e "\nFiles in the $CERTSPATH:\n"	
-	ls -1 ${CERTSPATH}
-	echo	
-    echo -e "------------"
     echo "Enter file name of the $CERT_ISSUER certificate:"
 	read CERTS[$i]
+	if [ ! -f "${CERTSPATH}${CERTS[$i]}" ]; then
+		echo -e "${COL_RED}File not found!${COL_RESET}"
+		exit 1
+	fi
 	echo 
     echo -e "Details of the certificate ${CERTS[$i]}:\n------------"
 	CERTINFO=`openssl x509 -in "${CERTSPATH}${CERTS[$i]}" -noout -subject -issuer -dates -serial`
 	echo "$CERTINFO"
 	C_SIGALG=`openssl x509 -in "${CERTSPATH}${CERTS[$i]}" -noout -text | grep -m 1 "Signature Algorithm:" | sed -e 's/^ *//g' -e 's/^Signature Algorithm: //'`
-	echo -e "Signature Algorithm = $C_SIGALG"
-    echo -e "------------"
-
+	
 	# check AM-26699 
 	if [ "$C_SIGALG" != "sha1WithRSAEncryption" ]; then 
-		echo -e "Only sha1WithRSAEncryption signature algorithm supported. Exiting."
+		echo -e "signature algorithm =${COL_RED} $C_SIGALG ${COL_RESET}\n"
+		echo -e "${COL_RED}Error: Only sha1WithRSAEncryption signature algorithm supported.$COL_RESET Exiting."
 		exit 1
+	else 
+		echo -e "signature algorithm = $C_SIGALG\n"
 	fi	
+    echo -e "------------"
+
 	C_ISSUER=`echo "$CERTINFO" | grep "issuer=" | sed 's/^issuer= //'`
 	C_SUBJECT=`echo "$CERTINFO" | grep "subject=" | sed 's/^subject= //'`
 	
 	# Check the chain (if this Certificate is issuer of the previous certificate)
-	if [ [ -n "$C_ISSUER" ] && [ -n "$C_SUBJECT" ] && [ "$CERT_ISSUER" = "$C_SUBJECT" ] ]; then
+	if [ -n "$C_ISSUER" ] && [ -n "$C_SUBJECT" ] && [ "$CERT_ISSUER" = "$C_SUBJECT" ]; then
 			echo -e "The certificate ${CERTS[0]} is signed by ${CERTS[$i]}.\nChecking if there is another CA in the chain ..." 
 			# Check if the CA certificate is self signed, assuming that Root CA must be self signed
 			if [ "$C_ISSUER" != "$C_SUBJECT" ]; then 
@@ -486,42 +522,37 @@ while [ $FULLCHAIN -eq 0 ]; do
 				CERT_ISSUER=$C_ISSUER
 				((i++))				
 			else	
-				echo -e "The ${CERTS[i]} is the Root CA certificate.\n"	
-				echo -e "#######################################"
-				echo -e "####### Done! Full chain found! #######" 
-				echo -e "#######################################\n"	
+				echo -e "OK. The ${CERTS[i]} is the Root CA certificate.\n"
+				echo -e ${COL_GREEN}"Done! Full chain found! ${COL_RESET} \n" 
 				FULLCHAIN=1
 			fi
 	else 
 		echo "${CERTS[$i]} is not issuer of ${CERTS[$i-1]}"
 		promptYesNo "Do you have another CA certificate to try?" y
 		if [ $? -eq 0 ]; then
-			echo "Unable to build the full chain. Exiting."
+			echo -e ${COL_RED}"Error: ${COL_RESET} Unable to build the full chain. Exiting."
 			exit 1
 		fi 	
 	fi
 done
 
+###################
+# Full chain found
 
 for i in "${CERTS[@]}"
 do    # List all the elements in the array.
   echo "${i}"
   echo "----------------------------------"
   openssl x509 -in "${CERTSPATH}${i}" -noout -subject -issuer -dates -serial
-  echo -e "----------------------------------\n" ;
+  echo -e "----------------------------------\n"
 done
 
- 
 promptYesNo "Import the above certificates into the appliance?" y
 	if [ $? -eq 0 ]; then
 		echo "Exiting."
 		exit 1
 	fi 
 		
-# Checking some known issues
-#
-# check AM-26699
-# 		
 		
 import_certs "$ALIAS"
 
@@ -530,4 +561,4 @@ list_certs "$ALIAS"
 configureServers "$ALIAS"
 
   
-echo "The certificate replacement successful."
+echo -e ${COL_GREEN}"The certificate replacement successful."${COL_RESET}
